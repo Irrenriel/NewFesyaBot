062f49cfb6bcdf217a1a27f47fc66bd0ade73fca
Irrenriel <yuronsuper@gmail.com>
2021-12-10 03:42:37 +0300

    Some changes.

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ async def startup_func(dp: Dispatcher):
     if not con:
         raise Exception('Can not connect to Database')
 
-    # result = await db.fetch('SELECT * FROM users', one_row=True)
-    # print('Result:', UserData(**result))
-    print('< < < Bot is working! > > >')
+    result = await db.fetch('SELECT * FROM users', one_row=True)
+    print('Result:', UserData(**result))
+    print('<- <- <- Bot is working! -> -> ->')
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True, loop=loop, on_startup=startup_func)
