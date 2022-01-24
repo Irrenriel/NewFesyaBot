8fd0f74dd7eb4911a482a42339f91f0ea74807cb
Irrenriel <yuronsuper@gmail.com>
2022-01-24 18:58:07 +0300

    Some fixes.

---
+++ example_config.py
@@ -0,0 +1,34 @@
+from sys import argv
+
+
+# Development:
+# Switch develop instances by this vars
+debug: bool = "--debug" in argv  # Not CAPS because logging.DEBUG
+
+
+# Variables to connect:
+# Aiogram Bot
+BOT_TOKEN: str = 'BOT TOKEN'
+PARSE_MODE: str = 'HTML'
+
+# Databases
+USER: str = 'USER'
+PASSWORD: str = 'PASSWORD'
+DATABASE: str = 'DB NAME'
+HOST: str = 'IP HOST'
+
+POSTGRES_DB = ('main', USER, PASSWORD, DATABASE, HOST)
+
+# Telethon
+SESSION_NAME: str = 'SESSION_NAME_test' if debug else 'SESSION_NAME_work'  # Example how to user debug mode
+API_ID: int = 1111111
+API_HASH: str = 'API_HASH'
+
+TELETHON_VARS = (SESSION_NAME, API_ID, API_HASH)
+
+
+# Other variables:
+# Roles
+ADMINS_ID: list[int] = []
+
+# Global constants:
